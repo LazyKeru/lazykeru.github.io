aade3fb711f9b8cae778577a12fa077e6d1826b0
LazyKeru <killian.allaire@hotmail.com>
2022-12-06 01:47:58 -0500

    feat: added routing

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,19 @@
 <template>
   <Header></Header>
-  <div v-for="(internship, index) in internships" :key="index" class="m-8">
-    <Professional :title="internship.title" :duration="internship.duration" :description="internship.description" :tags="internship.tags"/>
-  </div>
+  <Navbar></Navbar>
+  <router-view></router-view>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-import Professional from './components/Professional.vue';
+import Navbar from './pages/Navbar.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
-    Professional
-  },
-  data() {
-        return {
-            internships : [
-                {
-                  title: "DevOps - Claranet",
-                  duration: "2022 (5 mois)",
-                  description: "Déploiement de différentes applications OpenSource dans un environnement k8s sur Azure : fournisseur OpenID Connect, solution de workflow NoOPS, et suite de recherche et d'analyse",
-                  tags: [
-                    {
-                      icon: "pi pi-sitemap",
-                      text: "K8s"
-                    },
-                    {
-                      icon: "pi pi-cloud",
-                      text: "Azure"
-                    }
-                  ]
-                },
-                {
-                  title: "DevSecOps - Colas ",
-                  duration: "2021 (4 mois)",
-                  description: "Accompagnement d'un Audit de sécurité et préparation de la mise en place d'un WAF (pare-feu pour serveur Web). Mise en place d'un programme pour détecter les secrets codés en dur, et déploiement K8s",
-                  tags: [
-                    {
-                      icon: "",
-                      text: ""
-                    }
-                  ]
-                },
-                {
-                  title: "Développeur - Bouygues Telecom",
-                  duration: "2020 (4 mois)",
-                  description: "Développement d'un tableau de bord sur les usage des espaces clients mobiles de l'opérateur en ReactNative, avec implémentation des méthodes CI/CD",
-                  tags: [
-                    {
-                      icon: "",
-                      text: ""
-                    }
-                  ]
-                },
-                {
-                  title: "Caporal - Réserviste de l’armée de Terre",
-                  duration: "2018-2023",
-                  description: "Réalisation de diverses missions en tant que réserviste au sein du 3ème régiment d'infanterie de marine. Encadrant lors de stages (ex: PMT). Dont le rôle de CDE et CDG pendant des exercices.",
-                  tags: [
-                    {
-                      icon: "",
-                      text: ""
-                    }
-                  ]
-                }
-            ]
-        }
-    },
+    Navbar
+  }
 }
 </script>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,14 @@
 <template>
-    <div>
-        <!--<img src="../assets/profile.jpg" alt="" class="border-round-md shadow-8">-->
-        <p class="text-3xl font-bold">Killian ALLAIRE</p>
-        <div>
-            <div class="flex justify-content-around flex-wrap">
-                <Contact icon="pi pi-github" link="https://github.com/LazyKeru" information="LazyKeru" />
-                <Contact icon="pi pi-linkedin" link="https://fr.linkedin.com/in/killian-allaire-672756154" information="Killian ALLAIRE" />
-                <Contact icon="pi pi-envelope" link="" information="killian.allaire@hotmail.com" />
+    <div class="m-5 flex flex-1 justify-content-center">
+        <div class="grid grid-nogutter">
+            <img src="../assets/profile.jpg" alt="" class="border-circle h-15rem shadow-8"/>
+            <div>
+                <p class="text-3xl font-bold">Killian ALLAIRE</p>
+                <div class="ml-6">
+                    <Contact icon="pi pi-github" link="https://github.com/LazyKeru" information="LazyKeru" />
+                    <Contact icon="pi pi-linkedin" link="https://fr.linkedin.com/in/killian-allaire-672756154" information="Killian ALLAIRE" />
+                    <Contact icon="pi pi-envelope" link="" information="killian.allaire@hotmail.com" />
+                </div>
             </div>
         </div>
     </div>
