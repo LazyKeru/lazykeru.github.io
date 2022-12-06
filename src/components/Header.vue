9a0618d1907bfc0ffa4315a02c9f967988432b82
LazyKeru <killian.allaire@hotmail.com>
2022-12-06 00:19:57 -0500

    feat: added simple experiences widget

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <!--<img src="../assets/profile.jpg" alt="" class="border-round-md shadow-8">-->
+        <p class="text-3xl font-bold">Killian ALLAIRE</p>
+        <div>
+            <div class="flex justify-content-around flex-wrap">
+                <Contact icon="pi pi-github" link="https://github.com/LazyKeru" information="LazyKeru" />
+                <Contact icon="pi pi-linkedin" link="https://fr.linkedin.com/in/killian-allaire-672756154" information="Killian ALLAIRE" />
+                <Contact icon="pi pi-envelope" link="" information="killian.allaire@hotmail.com" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Contact from './Contact';
+
+export default {
+    name: "my-header",
+    components: {
+        Contact
+    }
+}
+</script>
---
+++ src/components/Professional.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="p-5 bg-primary-reverse shadow-5 border-round-xl fadein animation-duration-1000">
+        <div class="grid">
+            <div class="col-5 justify-content-start">
+                <p class="text-left text-xl font-semibold">
+                    {{title}}
+                </p>
+            </div>
+            <div class="col-6 justify-content-start">
+                <p class="text-center text-xl font-medium">
+                    {{duration}}
+                </p>
+            </div>
+        </div>
+        <div class="grid">
+            <div>
+                <p class="col text-justify text-base"> 
+                    {{description}}
+                </p>
+            </div>
+        </div>
+        <div class="grid">
+            <div v-for="(tag, index) in tags" :key="index" class="mr-3">
+                <p-chip :label="tag.text" :icon="tag.icon"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "my-experiences",
+    props: {
+        title: String,
+        duration: String,
+        description: String,
+        tags: Object,
+    },
+}
+</script>
