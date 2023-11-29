7ee10e4c842d38bb186c51b359d4274f84c0a4db
Killian ALLAIRE <killian.allaire@hotmail.com>
2023-11-29 22:33:24 +0100

    Feat/search component (#9)
    * Base of the search component in protfolio
    
    * added the tags for the search components
    
    * Returned tags for the search are unique
    
    * simple tag search
    
    * filter is done

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -3,7 +3,13 @@
     <Loading/>
   </div>
   <div v-else>
-    <div v-for="(project, index) in projects" :key="index" class="m-4 md:m-8">
+    <Search
+      class="p-5"
+      :tags="tags"
+      v-model:selectedTags="selectedTags"
+      v-model:searchArray="searchArray"
+    />
+    <div v-for="(project, index) in filteredProjects" :key="index" class="m-4 md:m-8">
       <a :href="project.link" class="no-underline">
         <Project 
             :title="project.title" 
@@ -23,18 +29,22 @@ import { defineComponent } from 'vue';
 import Loading from '@/components/Loading.vue';
 import Project from '@/components/Project.vue';
 import Backend from '@/service/backend'
-import { IProject } from '@/service/backend'
+import Search from '@/components/Search.vue';
+import { IProject, ITag } from '@/service/backend'
 
 export default defineComponent({
     name: "page-projects",
     components: {
+        Search,
         Project,
         Loading
     },
     data() {
       return {
         projects: [] as IProject[],
-        loading: true
+        loading: true,
+        selectedTags: [] as ITag[],
+        searchArray: ''
       }
     },
     mounted() {
@@ -42,6 +52,44 @@ export default defineComponent({
         this.projects = response
         this.loading = false
       })
+    },
+    methods: {
+      filteredProjectsByTag(projects:IProject[]): IProject[] {
+        if (this.selectedTags.length === 0) {
+          return projects
+        }
+        return projects.filter((project) => {
+          return project.tags.some((tag) => {
+            return this.selectedTags.some((selectedTag) => {
+              return selectedTag.text === tag.text
+            })
+          })
+        })
+      },
+      filteredProjectsByArray(projects:IProject[]): IProject[] {
+        if (this.searchArray.length === 0) {
+          return projects
+        }
+        return projects.filter((project) => {
+          return project.title.toLowerCase().includes(this.searchArray.toLowerCase()) || project.description.toLowerCase().includes(this.searchArray.toLowerCase())
+        })
+      }
+    },
+    computed: {
+      tags(): ITag[] {
+        return this.projects.map((project) => {
+          return project.tags
+        })
+        .flat()
+        .filter(
+          (element, index, self) => {
+            return index === self.findIndex(tag => tag.text === element.text)
+          }
+        )
+      },
+      filteredProjects(): IProject[] {
+        return this.filteredProjectsByArray(this.filteredProjectsByTag(this.projects))
+      }
     }
 })
 </script>
